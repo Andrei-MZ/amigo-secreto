4b711ec264ba699d9e48e7f3f0fd02519ca0516e
andrei <andreimendes8@gmail.com>
2025-02-03 20:07:24 -0300

    pending spiner

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -7,14 +7,14 @@ import { Input } from "./ui/input"
 import { Label } from "./ui/label"
 import { login, LoginState } from "@/app/(auth)/login/actions"
 import { Alert, AlertDescription, AlertTitle } from "./ui/alert"
-import { MessageCircle } from "lucide-react"
+import { Loader, MessageCircle } from "lucide-react"
 
-export default function LoginForm () {
+export default function LoginForm() {
     const [state, formAction, pending] = useActionState<LoginState, FormData>(
         login,
         {
-         success: null,
-         message: "",
+            success: null,
+            message: "",
         }
     )
 
@@ -26,46 +26,47 @@ export default function LoginForm () {
                     Digite seu email para receber um link de login.
                 </CardDescription>
             </CardHeader>
-                <CardContent>
-                    <form action="formAction">
-                        <div className="grid gap-4">
-                            <div className="grid gap-2">
+            <CardContent>
+                <form action="formAction">
+                    <div className="grid gap-4">
+                        <div className="grid gap-2">
                             <Label htmlFor="email">Email</Label>
-                            <Input 
-                            id="email" 
-                            type="email" 
-                            name="email" 
-                            placeholder="joão_exemplo@gmail.com" 
-                            required 
+                            <Input
+                                id="email"
+                                type="email"
+                                name="email"
+                                placeholder="joão_exemplo@gmail.com"
+                                required
                             />
-                            </div>
-
-                            {state.success === true && (
-                                <Alert className="text-muted-foreground">
-                                    <MessageCircle className="h-4 w-4 !text-green-600" />
-                                    <AlertTitle className="text-gray-50">Email enviado!</AlertTitle>
-                                    <AlertDescription>
-                                        Confira seu inbox para acessar o link de login.
-                                    </AlertDescription>
-                                </Alert>
-                            )}
-
-                            {state.success === false && (
-                                <Alert className="text-muted-foreground">
-                                    <MessageCircle className="h-4 w-4 !text-red-600" />
-                                    <AlertTitle className="text-gray-50">Erro!</AlertTitle>
-                                    <AlertDescription>
-                                        Ocorreu um erro ao enviar o link de login. Por favor, entre em contato com o suporte!
-                                    </AlertDescription>
-                                </Alert>
-                            )}
-
-                            <Button type="submit" className="w-full">
-                            Login
-                            </Button>
                         </div>
-                    </form>
-                </CardContent>
+
+                        {state.success === true && (
+                            <Alert className="text-muted-foreground">
+                                <MessageCircle className="h-4 w-4 !text-green-600" />
+                                <AlertTitle className="text-gray-50">Email enviado!</AlertTitle>
+                                <AlertDescription>
+                                    Confira seu inbox para acessar o link de login.
+                                </AlertDescription>
+                            </Alert>
+                        )}
+
+                        {state.success === false && (
+                            <Alert className="text-muted-foreground">
+                                <MessageCircle className="h-4 w-4 !text-red-600" />
+                                <AlertTitle className="text-gray-50">Erro!</AlertTitle>
+                                <AlertDescription>
+                                    Ocorreu um erro ao enviar o link de login. Por favor, entre em contato com o suporte!
+                                </AlertDescription>
+                            </Alert>
+                        )}
+
+                        <Button type="submit" className="w-full">
+                            {pending && <Loader className="animate-spin" />}
+                            Login
+                        </Button>
+                    </div>
+                </form>
+            </CardContent>
         </Card>
     )
 }
