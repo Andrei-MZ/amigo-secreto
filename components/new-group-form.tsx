b2de6040a99b386e2b55c042ae322699322508db
andrei <andreimendes8@gmail.com>
2025-02-16 15:09:35 -0300

    atualizando o valor do index (pessoas)

--- components/new-group-form.tsx
+++ components/new-group-form.tsx
@@ -1,6 +1,9 @@
 'use client'
 
-import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { useState } from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 
 interface Participant {
     name: string;
@@ -13,10 +16,20 @@ export default function NewGroupForm({ loggedUser,
      loggedUser: { email: string; id: string }
 }) {
     const [participants, setParticipants] = useState<Participant[]>([
-        { name: "", email: loggedUser.email}
+        { name: "", email: loggedUser.email },
     ])
 
     const [groupName, setGroupName] = useState("")
+
+    function updatedParticipante(
+        index: number, 
+        field: keyof Participant, 
+        value: string
+    ) {
+        const updatedParticipants = [...participants]
+        updatedParticipants[index][field] = value
+        setParticipants(updatedParticipants)
+    }
 
     return (
         <Card className=" w-full max-w-2xl mx-auto">
@@ -24,6 +37,38 @@ export default function NewGroupForm({ loggedUser,
                 <CardTitle>Novo grupo</CardTitle>
                 <CardDescription>Convide seus amigos para participar</CardDescription>
             </CardHeader>
+            <form action={() => {}}>
+                <CardContent className="space-y-4">
+                    <div className="space-y-2">
+                        <Label htmlFor="group-name">Nome do grupo</Label>
+                        <Input
+                            id="group-name"
+                            name="group-name"
+                            value={groupName}
+                            onChange={(e) => setGroupName(e.target.value)}
+                            placeholder="Digite o nome do grupo"
+                            required
+                        />
+                    </div>
+
+                    <h2 className="!mt-12">Participantes</h2>
+                    {participants.map((participant, index) => (
+                        <div key={index} className="flex flex-col md:flex-row items-end space-y-4 md:space-y-0 md:space-x-4"
+                        >
+                            <div className="flex-grow space-y-2 w-full">
+                                <Label htmlFor={`name-${index}`}>Nome</Label>
+                                <Input id={`name-${index}`} 
+                                name="name" 
+                                value={participant.name} 
+                                onChange={(e) => {
+                                    updatedParticipante(index, "name", e.target.value)
+                                }} />
+                            </div>
+                        </div>
+                    ))}
+                </CardContent>
+            </form>
+
         </Card>
     )
 
