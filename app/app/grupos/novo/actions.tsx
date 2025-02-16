2e73ca5ffd72801f26b5dd4538ec609ba53b95cb
andrei <andreimendes8@gmail.com>
2025-02-16 16:43:17 -0300

    auth error configuração

--- app/app/grupos/novo/actions.tsx
+++ app/app/grupos/novo/actions.tsx
@@ -1,4 +1,28 @@
+import { createClient } from "@/utils/supabase/server";
+
 export type CreateGroupState = {
     success: null | boolean;
     message?: string;
+}
+
+export async function createGroup(
+    _previousState: CreateGroupState,
+    formData: FormData
+) {
+    const supabase = await createClient()
+
+    const { data: authUser, error: authError } = await supabase.auth.getUser()
+
+    if (authError) {
+        return{
+            succeso: false,
+            message: "Ocorreu um erro ao criar o grupo"
+        }
+    }
+
+    const names = formData.getAll("name")
+    const email = formData.getAll("email")
+    const groupName = formData.getAll("groupName")
+
+    
 }
