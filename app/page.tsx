df352b1d5f6f5207417f0d158b01cf41d0477d2e
andrei <andreimendes8@gmail.com>
2025-03-01 15:03:54 -0300

    finalizada alterações de redirecionamento

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <div className="bg-background w-screen h-screen flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-4">App Amigo Secreto</h1>
-      <Link href="/app/grupos/novo">
+      <Link href="/login">
         <button className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 transition">
-          Criar Novo Grupo
+          Página de Login
         </button>
       </Link>
     </div>
